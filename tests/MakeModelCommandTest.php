a7476bde74b349f8d701114d350eff085fc1c4b2
solid <myappgo123@gmail.com>
2022-04-01 02:39:13 +0430

    Added test

--- tests/MakeModelCommandTest.php
+++ tests/MakeModelCommandTest.php
@@ -1,12 +1,16 @@
 <?php
 
 use Mkhab7\MagicalArtisan\Console\Commands\MagicalModelMakeCommand;
+use Mkhab7\MagicalArtisan\Tests\TestCase;
 
-test('example', function () {
-/** @var \Mkhab7\MagicalArtisan\Tests\TestCase $this */
+test('creating a few new model', function () {
+    /** @var TestCase $this */
     $this->artisan(
-        MagicalModelMakeCommand::class,['name'=>'User Model -a -m']
+        MagicalModelMakeCommand::class,
+        ['names' => ['Model1', 'Model2', 'Model3']]
     )->assertSuccessful()
-        ->expectsOutput('created');
+        ->expectsOutput('Creating Model1 model...')
+        ->expectsOutput('Creating Model2 model...')
+        ->expectsOutput('Creating Model3 model...');
 });
 
